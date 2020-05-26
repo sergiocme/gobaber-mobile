958d413f9983ca3287f93ccdefb672a00ae03ba2
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-05-26 20:08:11 -0300

    Add RobotoSlab font family

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container } from './styles';
+
+import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
@@ -8,6 +9,7 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImg} />
+      <Title>SignIn</Title>
     </Container>
   );
 };
