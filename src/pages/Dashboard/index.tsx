967a1d0a9e109ad71d7ffd16a8b7c1984a4b72d6
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-17 12:07:06 -0300

    Fetch providers list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-// import {  } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
 import { useAuth } from '../../contexts/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,12 +10,32 @@ import {
   UserName,
   ProfileButton,
   UserAvatar,
+  ProvidersList,
 } from './styles';
 
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 const Dashboard: React.FC = () => {
+  const [providers, setProviders] = useState<Provider[]>([]);
+
   const {
+    signOut,
     data: { user },
   } = useAuth();
+
+  useEffect(() => {
+    async function loadProviders(): Promise<void> {
+      const { data } = await api.get('/providers');
+
+      setProviders(data);
+    }
+
+    loadProviders();
+  }, []);
 
   return (
     <Container>
@@ -25,10 +45,16 @@ const Dashboard: React.FC = () => {
           <UserName>{user.name}</UserName>
         </HeaderTitle>
 
-        <ProfileButton onPress={() => {}}>
+        <ProfileButton onPress={signOut}>
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
       </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
