3a6907993d96ec18d177368bb7c60f40877cfb1e
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-18 14:37:17 -0300

    Add horizontal provider list on CreateAppointment

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 import { useAuth } from '../../contexts/auth';
+import api from '../../services/api';
 
 import {
   Container,
@@ -10,10 +11,21 @@ import {
   BackButton,
   HeaderTitle,
   UserAvatar,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
 } from './styles';
 
 interface RouteParams {
   providerId: string;
+}
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
 }
 
 const CreateAppointment: React.FC = () => {
@@ -22,11 +34,23 @@ const CreateAppointment: React.FC = () => {
   } = useAuth();
   const { goBack } = useNavigation();
   const route = useRoute();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
   const { providerId } = route.params as RouteParams;
 
   const navigateBack = useCallback(() => {
     goBack();
   }, [goBack]);
+
+  useEffect(() => {
+    async function loadProviders(): Promise<void> {
+      const { data } = await api.get('/providers');
+
+      setProviders(data);
+    }
+
+    loadProviders();
+  }, []);
 
   return (
     <Container>
@@ -39,6 +63,21 @@ const CreateAppointment: React.FC = () => {
 
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      <ProvidersListContainer>
+        <ProvidersList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          data={providers}
+          keyExtractor={provider => provider.id}
+          renderItem={({ item }) => (
+            <ProviderContainer>
+              <ProviderAvatar source={{ uri: item.avatar_url }} />
+              <ProviderName>{item.name}</ProviderName>
+            </ProviderContainer>
+          )}
+        />
+      </ProvidersListContainer>
     </Container>
   );
 };
--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { Provider } from './index';
 
 export const Container = styled.View``;
 
@@ -24,4 +26,34 @@ export const UserAvatar = styled.Image`
   height: 56px;
   margin-left: auto;
   border-radius: 28px;
+`;
+
+export const ProvidersListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProvidersList = styled(FlatList as new () => FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-right: 16px;
+  padding: 8px 12px;
+  border-radius: 10px;
+  background: #3e3b47;
+`;
+
+export const ProviderAvatar = styled.Image`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text`
+  margin-left: 8px;
+  font-family: 'RobotSlab-Medium';
+  font-size: 16px;
+  color: #f4ede8;
 `;
