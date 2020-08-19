0d0c75b7f8859e732d3e70ceb044d6c027f92d9a
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-19 15:37:39 -0300

    Format date on CreatedAppointment screen

--- src/pages/CreatedAppointment/index.tsx
+++ src/pages/CreatedAppointment/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
-import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
 import Icon from 'react-native-vector-icons/Feather';
 
 import {
@@ -10,8 +11,15 @@ import {
   OkayButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
+
 const CreatedAppointment: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOkayButton = useCallback(() => {
     reset({
@@ -20,13 +28,17 @@ const CreatedAppointment: React.FC = () => {
     });
   }, [reset]);
 
+  const formattedDate = useMemo(() => {
+    return format(routeParams.date, "EEEE', day' dd', 'MMMM', 'HH:mm'h'");
+  }, [routeParams.date]);
+
   return (
     <Container>
       <Icon name="check" size={128} color="#04d361" />
 
       <Title>Success</Title>
       <Description>
-        You now have a meet with your Barber at Setemper, 14 on 09:00
+        You now have a meet with your Barber at {formattedDate}.
       </Description>
 
       <OkayButton onPress={handleOkayButton}>
