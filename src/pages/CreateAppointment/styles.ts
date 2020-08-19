9a29dad3844ab60b925e4a813badd2ed615c776d
Sérgio Carvalho <sergiocarvalho.m@outlook.com>
2020-08-19 14:03:56 -0300

    Fix Container flex to ScrollView work

--- src/pages/CreateAppointment/styles.ts
+++ src/pages/CreateAppointment/styles.ts
@@ -16,7 +16,9 @@ interface HourTextProps {
   selected: boolean;
 }
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
 
 export const Header = styled.View`
   flex-direction: row;
